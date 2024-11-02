0302d74704c075250c988b0bd033b236ce17488b
Jon Alamar <36095904+JonAlamar@users.noreply.github.com>
2024-11-02 00:40:19 -0400

    Task 1

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -7,3 +7,15 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_subnet" "variable-subnet" {
+  vpc_id                  = aws_vpc.vpc_id
+  cidr_block              = "10.0.250.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "sub-variables-us-east-1a"
+    Terraform = "true"
+  }
+}
