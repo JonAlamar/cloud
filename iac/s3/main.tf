6b28ff84f05d2f416fee6e92b39917edf5d271c4
Jon Alamar <36095904+JonAlamar@users.noreply.github.com>
2024-11-02 12:49:03 -0400

    update

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -9,13 +9,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "<AMI>"
+  ami           = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
 
-  subnet_id              = "<SUBNET>"
-  vpc_security_group_ids = ["<SECURITY_GROUP>"]
+  subnet_id              = "subnet-0d8d614e45a6b6b02"
+  vpc_security_group_ids = ["sg-0ae35beb5cf748183"]
 
   tags = {
-    "Identity" = "<IDENTITY>"
+    "Identity" = "Test Identity Tag"
   }
 }
