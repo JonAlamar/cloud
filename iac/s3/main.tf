ac1b0d3371fa5f9b14c29b9f68db65f1b1593862
Jon Alamar <36095904+JonAlamar@users.noreply.github.com>
2024-11-02 11:23:58 -0400

    cleaning and removing keys

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -8,14 +8,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_subnet" "variable-subnet" {
-  vpc_id                  = aws_vpc.vpc_id
-  cidr_block              = "10.0.250.0/24"
-  availability_zone       = "us-east-1"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name      = "sub-variables-us-east-1a"
-    Terraform = "true"
-  }
+resource "random_string" "random" {
+  length = 16
 }
