2880f7f756a88fc3fb9dca3806bbbba32100eba0
Jon Alamar <36095904+JonAlamar@users.noreply.github.com>
2024-11-02 22:08:07 -0400

    update

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -152,7 +152,7 @@ resource "aws_security_group" "my-new-security-group" {
 }
 
 resource "aws_subnet" "variables-subnet" {
-  vpc_id                  = aws_vpc.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.variables_sub_cidr
   availability_zone       = var.aws_region
   map_public_ip_on_launch = var.variables_sub_auto_ip
