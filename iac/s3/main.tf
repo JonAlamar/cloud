bffafafab391d08259bb9d205e520b0d8df86d39
Jon Alamar <36095904+JonAlamar@users.noreply.github.com>
2024-11-02 22:01:38 -0400

    update

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -150,3 +150,15 @@ resource "aws_security_group" "my-new-security-group" {
     Purpose = "Intro to Resource Blocks Lab"
   }
 }
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.id
+  cidr_block              = "10.0.250.0.24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "sub-variables-us-east-1a"
+    Terraform = "true"
+  }
+}
