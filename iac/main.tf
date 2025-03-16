57e66bf57a6d9a47c3a147f54ca7716512a6211d
JonAlamar <none>
2025-03-16 14:26:46 -0400

    test bucket

---
+++ iac/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket-${terraform.workspace}"  # Unique name per workspace
+}
---
+++ iac/provider.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  region = "us-east-1"  # Match the workflow region
+}
